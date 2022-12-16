9d930c5f3f91185a4bcba9f61e006deb3152cf32
Vovchick4 <vovaspodpivas@gmail.com>
2022-12-16 04:12:06 +0200

    D

--- database/seeders/SuperAdminsTableSeeder.php
+++ database/seeders/SuperAdminsTableSeeder.php
@@ -15,18 +15,18 @@ class SuperAdminsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert(array (
+        DB::table('users')->insert(array(
             0 =>
-                array (
-                    'email' => 'superadmin@admin.com',
-                    'name' => 'Super',
-                    'lastname' => 'Admin',
-                    'image' => 'users/default.png',
-                    'password' => '$2y$10$E1.y.jjmr7ecmMAiwFFQMO6KXn0scLB2hvVBbx8cny.lREZ6xdDMW', //JnR390XB
-                    'remember_token' => 'dvFDbgrDJ1SZPQPBgBUtmv3Rpobcm9NHgHyDT9Ta5ESJqiPeaYhzNZ2cnX3m',
-                    'role' => 'super-admin',
-                    'restaurant_id' => 1,
-                ),
+            array(
+                'email' => 'superadmin@admin.com',
+                'name' => 'Super',
+                'lastname' => 'Admin',
+                'image' => 'users/default.png',
+                'password' => '$2a$08$i3sG4lT7cVHzfbxX./giCu2CtBY0cjvpyYrUQy1yGFpL7B2rKv/O.', //JnR390XB USE BCRYPT MethoD
+                'remember_token' => 'dvFDbgrDJ1SZPQPBgBUtmv3Rpobcm9NHgHyDT9Ta5ESJqiPeaYhzNZ2cnX3m',
+                'role' => 'super-admin',
+                'restaurant_id' => 1,
+            ),
         ));
     }
 }
