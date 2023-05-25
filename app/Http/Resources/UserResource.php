227ed4aa87f1d95eb1a74c7ef222b806cb90a668
Vovchick4 <vovaspodpivas@gmail.com>
2023-05-25 16:51:13 +0300

    fix

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -22,6 +22,8 @@ class UserResource extends JsonResource
             'email' => $this->email,
             'phone' => $this->phone,
             'role' => $this->role,
+            'remember_token' => $this->remember_token,
+            'email_verify_at' => $this->email_verified_at,
             'restaurant_id' => $this->restaurant_id,
             'restaurant_slug' => $this->restaurant->slug
         ];
